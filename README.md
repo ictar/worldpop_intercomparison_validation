0ce98e2d4e650cc76947a97c31a885886af6e2c7
ictar <ele.qiong@gmail.com>
2020-12-28 23:18:33 +0800

    Add validation page 'valid.html'

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This is the web part of GIS lab project 2021 of Polimi GIS class.
 # how to use
 1. add `group_10` to geoserver
 2. modify configuration in `assets/js/map.js`
+3. modify the files in `assets/stats`
 
 ## how to visit
 use python to start a web HTTP server in the same folder:
--- assets/js/map.js
+++ assets/js/map.js
@@ -18,6 +18,26 @@ var intercomp_diff_info = new ol.source.ImageWMS({
     params: {'LAYERS': 'Lab_GIS:intercomp_diff'}
 });
 
+/* Reclassified GHS-POP and WorldPop in GeoTiff format */
+var ghs_rec_info = new ol.source.ImageWMS({
+    url: geoserver_url+'/Lab_GIS/wms',
+    params: {'LAYERS': 'Lab_GIS:ghs_rec_int'}
+});
+var worldpop_rec_info = new ol.source.ImageWMS({
+    url: geoserver_url+'/Lab_GIS/wms',
+    params: {'LAYERS': 'worldpop_rec_int'}
+});
+/* Map of differences (GHS-POP - WorldPop)+1 */
+var difference_3scenarios_info = new ol.source.ImageWMS({
+    url: geoserver_url+'/Lab_GIS/wms',
+    params: {'LAYERS': 'difference_3scenarios_int'}
+});
+/* Vector layer of classified sampling points in GeoPackage format with 3 attributes */
+var classified_points_url = geoserver_url+'/Lab_GIS/ows?service=WFS&' +
+'version=2.0.0&request=GetFeature&typeName=Lab_GIS:vector_classified_points&' +
+'outputFormat=text/javascript&srsname=EPSG:3857&'
+
+
 /* Basemaps */
 var osm = new ol.layer.Tile({
     title: 'OpenStreetMap',
@@ -91,6 +111,45 @@ var intercomp_diff = new ol.layer.Image({
     title: "Map of differences",
     source: intercomp_diff_info,
     opacity: 0.7
+});
+
+/* Reclassified GHS-POP and WorldPop in GeoTiff format */
+var ghs_rec = new ol.layer.Image({
+    title: "Reclassfied GHS-POP",
+    source: ghs_rec_info,
+    opacity: 0.7
+});
+var worldpop_rec = new ol.layer.Image({
+    title: "Reclassfied WorldPop",
+    source: worldpop_rec_info,
+    opacity: 0.7
+});
+/* Map of differences (GHS-POP - WorldPop)+1 */
+var difference_3scenarios = new ol.layer.Image({
+    title: "Map of differences",
+    source: difference_3scenarios_info,
+    opacity: 0.7
+});
+
+/* Vector layer of classified sampling points in GeoPackage format with 3 attributes */
+var vectorSource1 = new ol.source.Vector({
+	loader: function(extent, resolution, projection){
+		var url = classified_points_url +
+			'&format_options=callback:loadFeatures_CP';
+		$.ajax({
+			url: url,
+			dataType: 'jsonp'
+		});
+	}
+});
+function loadFeatures_CP(response) {
+    var geodata = new ol.format.GeoJSON();
+	vectorSource1.addFeatures(geodata.readFeatures(response));
+}
+var classified_points = new ol.layer.Vector({
+	title: 'Classified Sampling Points',
+    source: vectorSource1,
+	opacity: 0.8
 });
 
 /* Create the map */
@@ -255,4 +314,103 @@ map_intc.on('pointermove', function(event){
 	var pixel = map_intc.getEventPixel(event.originalEvent);
 	var hit = map_intc.hasFeatureAtPixel(pixel);
 	map_intc.getTarget().style.cursor = hit ? 'pointer' : '';
+})
+
+
+/*************** map in validation  *******************/
+var map_valid = new ol.Map({
+    target: document.getElementById('map2'),
+    layers: [
+        new ol.layer.Group({
+            title: 'Base Maps',
+            layers: [
+                stamenWatercolor,
+                stamenToner,
+                osm
+            ]
+        }),
+        new ol.layer.Group({
+            title: 'Overlay Layers',
+            layers: [
+                area_group,
+                ghs_rec,
+                worldpop_rec,
+                difference_3scenarios,
+                classified_points
+            ]
+        })
+    ],
+    view: new ol.View({
+        center:[26, 64],
+        zoom: 2
+    }),
+    controls: ol.control.defaults().extend([
+        new ol.control.ScaleLine(),
+        new ol.control.FullScreen(),
+        new ol.control.OverviewMap({
+            layers:[
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
+            ]
+        }),
+        new ol.control.MousePosition({
+            coordinateFormat: ol.coordinate.createStringXY(4),
+            projection: 'EPSG:4326'
+        })
+    ])
+});
+var layerSwitcher2 = new ol.control.LayerSwitcher({});
+map_valid.addControl(layerSwitcher2);
+
+/* Popup */
+var elementPopup2 = document.getElementById('popup2');
+
+var popup2 = new ol.Overlay({
+	element: elementPopup2
+});
+map_valid.addOverlay(popup2);
+
+map_valid.on('click', function(event){
+	var feature = map_valid.forEachFeatureAtPixel(event.pixel, function(feature, layer){
+		return feature;
+    });
+    //console.log(feature)
+
+	if(feature != null) {
+		var pixel = event.pixel;
+		var coord = map_valid.getCoordinateFromPixel(pixel);
+		popup2.setPosition(coord);
+        $(elementPopup2).attr('title', 'Classified Sampling Point');
+        var content = ''
+        if(feature.get('Classified') == '1') {
+            content += '<b>Classified: </b>Not Urban'
+        } else if(feature.get('Classified') == '2'){
+            content += '<b>Classified: </b>Urban'
+        }
+        if(feature.get('Thematic C') == '1') {
+            content += '</br><b>GHS Thematic Class: </b>Not Urban'
+        } else if(feature.get('Thematic C') == '2'){
+            content += '</br><b>GHS Thematic Class: </b>Urban'
+        }
+        if(feature.get('Worldpop_T') == '1') {
+            content += '</br><b>WorldPop Thematic Class: </b>Not Urban'
+        } else if(feature.get('Worldpop_T') == '2') {
+            content += '</br><b>WorldPop Thematic Class: </b>Urban'
+        }
+		$(elementPopup2).attr('data-content', content);
+        //console.log(elementPopup2)
+		$(elementPopup2).popover({'placement': 'top', 'html': true});
+		$(elementPopup2).popover('show');
+	}
+});
+
+map_valid.on('pointermove', function(event){
+	if(event.dragging) {
+		$(elementPopup2).popover('dispose');
+		return;
+	}
+	var pixel = map_valid.getEventPixel(event.originalEvent);
+	var hit = map_valid.hasFeatureAtPixel(pixel);
+	map_valid.getTarget().style.cursor = hit ? 'pointer' : '';
 })
